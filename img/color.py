946505ead4c77e649c99935118b3c6c34f055017
444lhc <444lhc@gmail.com>
2020-12-31 08:26:06 +0530

    Update streak color

---
+++ img/color.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+Color class for gen_streak
+Author: Sreejith S
+
+palletes: [kawanet/material-colors.json]
+"""
+
+from PIL import ImageColor
+import json
+
+class Color:
+    def __init__(self, palette="amber", alpha='ff'):
+        self._im_bg = '#ffffff'
+        self._cell_bg = '#e6e6e6'
+        self._cell_outline = '#ffffff'
+        self._alpha = alpha
+        self._c_ids = ['50', '100', '200', '300', '400',
+                       '500', '600', '700', '800', '900']
+
+        with open("material-colors.json", 'r') as fp:
+            palettes = json.load(fp)
+            color_dict = palettes[palette]
+            self._im_cell_fill_colors = [color_dict[c] for c in self._c_ids]
+
+    def get_im_bg(self):
+        return ImageColor.getrgb(self._im_bg + self._alpha)
+
+    def get_cell_bg(self):
+        return ImageColor.getrgb(self._cell_bg + self._alpha)
+
+    def get_cell_outline(self):
+        return ImageColor.getrgb(self._cell_outline + self._alpha)
+
+    def get_cell_bg_maprange(self, range, value):
+        # https://rosettacode.org/wiki/Map_range#Python
+        if value == 0:
+            return self.get_cell_bg()
+        else:
+            color_range = (0, len(self._im_cell_fill_colors)-1)
+            (r1, r2), (c1, c2) = range, color_range
+            color_id =  int(c1 + ((value - r1) * (c2 - c1) / (r2 - r1)))
+            return ImageColor.getrgb(self._im_cell_fill_colors[color_id] + self._alpha)
+
+
+
+
--- img/gen_streak.py
+++ img/gen_streak.py
@@ -6,30 +6,26 @@ Generate a streak chart from commit dates
 Author: Sreejith S
 """
 
-from PIL import Image, ImageDraw, ImageColor
+from PIL import Image, ImageDraw
 import subprocess
 from datetime import datetime, timedelta
 from collections import Counter
-from math import ceil
+from color import Color
+
 
 start_date = '2020-12-25' #yyyy-mm-dd
-date_format = '%Y-%m-%d'
 local_repos = ['/home/sj/Projects/ROS/ROS',
-	       '/home/sj/Projects/ROS/AMR_omnibot',
-	       '/home/sj/Projects/Code/2021/100-days-of-ROS']
+							 '/home/sj/Projects/ROS/AMR_omnibot',
+							 '/home/sj/Projects/Code/2021/100-days-of-ROS']
 
 # image options
+c = Color(palette='purple', alpha='ff')
 im_shape = (1001, 161)
 im_cell_width = 40
-im_bg = (255, 255, 255, 255)
-im_cell_fill_empty = (230, 230, 230, 255)
-colors = ['#b2dfdb', '#80cbc4', '#4db6ac', '#26a69a']
-alpha = 'ff' #200
-im_cell_fill_full = [ImageColor.getrgb(c + alpha) for c in colors]
-im_cell_outline = im_bg#ImageColor.getrgb('#00796b' + alpha)
 
 
 # Creating 100 days
+date_format = '%Y-%m-%d'
 sd = datetime.strptime(start_date, date_format)
 dates = [(sd + timedelta(days=i)).strftime(date_format) for i in range(100)]
 date_count = Counter(dates)
@@ -40,21 +36,13 @@ for cwd in local_repos:
     output = subprocess.run(cmd.split(), stdout=subprocess.PIPE, cwd=cwd)
     dates_stdout = output.stdout.decode('utf-8').split()
     date_count.update(dates_stdout)
-# print(date_count)
 
 
-def maprange(a, b, s):
-    # https://rosettacode.org/wiki/Map_range#Python
-    (a1, a2), (b1, b2) = a, b
-    return  b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
-
-
-img = Image.new('RGBA', im_shape, im_bg)
+img = Image.new('RGBA', im_shape, c.get_im_bg())
 draw = ImageDraw.Draw(img)
 iw = im_cell_width
 
-commit_range = (1, max(date_count.values()))
-color_range = (0, len(im_cell_fill_full)-1)
+commit_range = (0, max(date_count.values())-1)
 day = 0
 
 for j in range(4):
@@ -62,17 +50,10 @@ for j in range(4):
         date_str = dates[day]
         day += 1
         commit_count = date_count[date_str] - 1
-        if commit_count == 0:
-            color = im_cell_fill_empty
-            border_color = im_bg
-        else:
-            color_num = maprange(commit_range, color_range, commit_count)
-            color = im_cell_fill_full[ceil(color_num)]
-            border_color = im_cell_outline
+        color = c.get_cell_bg_maprange(commit_range, commit_count)
+        border_color = c.get_cell_outline()
         draw.rectangle([0+i*iw, 0+j*iw, iw+i*iw, iw+j*iw], fill=color,
                        outline=border_color, width=3)
 
-from PIL import ImageFilter
-img.filter(ImageFilter.GaussianBlur(10))
 img.save('streak.png', 'PNG')
 
